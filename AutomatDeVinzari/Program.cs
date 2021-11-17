a4f17f0d44c67271a939694296857bc4f7e28684
Ion Josanu <nicujosanu@yahoo.com>
2021-11-17 18:18:44 +0200

    a 2-a varianta, cu N, D si Q.

--- AutomatDeVinzari/Program.cs
+++ AutomatDeVinzari/Program.cs
@@ -8,17 +8,43 @@ namespace AutomatDeVinzari
         {
             Console.WriteLine("Acesta est un automat de vinzari!\n" +
                 "Produsul are o valoare de 20 de centi!\n" +
-               "Ce tip de monede doriti sa introduceti? De 1, 5, 10, 20, 50 centi?");
-            int centi1 = int.Parse(Console.ReadLine());
+               "Ce tip de monede doriti sa introduceti? N(nickel),D(dime) sau Q(quarter)?");
+            char moneda = char.Parse(Console.ReadLine());
+            int centi1 = 0;
             int centi2 = 0;
-            while (centi1<20)
+            switch (moneda)
             {
-                Console.WriteLine($"Sold introdus de {centi1} centi  insuficient!\n" +
-                    $"Mai introduceti monede:");
-                centi2 = int.Parse(Console.ReadLine());
+                case 'N':
+                    centi2 = 5;
+                    break;
+                case 'D':
+                    centi2 = 10;
+                    break;
+                case 'Q':
+                    centi2 = 15;
+                    break;
+            }
+            centi1 = centi1 + centi2;
+
+            while (centi1 < 20)
+            {
+                Console.WriteLine($"Credit insuficient de {centi1} centi .Mai introduceti monede:");
+                 moneda = char.Parse(Console.ReadLine());
+                switch (moneda)
+                {
+                    case 'N':
+                        centi2 = 5;
+                        break;
+                    case 'D':
+                        centi2 = 10;
+                        break;
+                    case 'Q':
+                        centi2 = 15;
+                        break;
+                }
                 centi1 = centi1 + centi2;
             }
-            if (centi1>=20)
+            if (centi1 >= 20)
             {
                 int rest = centi1 - 20;
                 Console.WriteLine($"Sold suficient de {centi1}\n" +
